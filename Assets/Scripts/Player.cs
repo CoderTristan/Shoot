980523ddea92570491c169bd361428fd3a609458
CoderTristan <tristan.grahl@outlook.com>
2026-05-11 16:22:32 -0400

    more movement almost finished

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,101 +1,62 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [Header("References")]
-    public Rigidbody rb;
-    public Transform cam;
-
-    [Header("Movement")]
-    public float groundAccel = 30f;
-    public float airAccel = 12f;
-    public float maxGroundSpeed = 14f;
-    public float maxAirSpeed = 22f;
-    public float friction = 6f;
-
-    [Header("Jumping")]
-    public float jumpForce = 12f;
-    public float groundCheckDistance = 1.1f;
-
-    [Header("Sliding")]
-    public float slideBoost = 1.3f;
-    public float slideFriction = 1f;
-    bool sliding;
-
-    float inputX, inputY;
-    bool grounded;
-
+    [SerializeField] private PlayerCharacter playerCharacter;
+    [SerializeField] private PlayerCamera playerCamera;
+    private InputSystem_Actions _inputActions;
     void Start()
     {
-        rb.freezeRotation = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        _inputActions = new InputSystem_Actions();
+        _inputActions.Enable();
+        playerCharacter.Initialize();
+        playerCamera.Initialize(playerCharacter.GetCameraTarget());
+    }
+
+    void OnDestroy()
+    {
+        _inputActions.Dispose();
     }
 
     void Update()
     {
-        inputX = Input.GetAxisRaw("Horizontal");
-        inputY = Input.GetAxisRaw("Vertical");
-
-        if (Input.GetKeyDown(KeyCode.LeftControl))
-            StartSlide();
-        if (Input.GetKeyUp(KeyCode.LeftControl))
-            StopSlide();
-
-        if (Input.GetKeyDown(KeyCode.Space) && grounded)
-            Jump();
-    }
-
-    void FixedUpdate()
-    {
-        grounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance);
-
-        if (grounded && !sliding)
-            ApplyFriction();
-
-        MovePlayer();
-    }
-
-    void MovePlayer()
-    {
-        Vector3 moveDir = (cam.forward * inputY + cam.right * inputX).normalized;
-        moveDir.y = 0;
-
-        float accel = grounded ? groundAccel : airAccel;
-        float maxSpeed = grounded ? maxGroundSpeed : maxAirSpeed;
-
-        // Add acceleration
-        rb.linearVelocity += moveDir * accel * Time.fixedDeltaTime;
-
-        // Clamp horizontal speed
-        Vector3 horizontalVel = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
-        if (horizontalVel.magnitude > maxSpeed)
+        var input = _inputActions.Player;
+        var deltaTime = Time.deltaTime;
+        var cameraInput = new CameraInput
         {
-            horizontalVel = horizontalVel.normalized * maxSpeed;
-            rb.linearVelocity = new Vector3(horizontalVel.x, rb.linearVelocity.y, horizontalVel.z);
+            Look = input.Look.ReadValue<Vector2>()
+        };
+        playerCamera.UpdateRotation(cameraInput);
+
+        var characterInput = new CharacterInput
+        {
+            Rotation = playerCamera.transform.rotation,
+            Move = input.Move.ReadValue<Vector2>(),
+            Jump = input.Jump.WasPressedThisFrame(),
+            JumpSustain = input.Jump.IsPressed(),
+            Crouch = input.Crouch.WasPressedThisFrame() ? CrouchInput.Toggle : CrouchInput.None
+        };
+        playerCharacter.UpdateInput(characterInput);
+        playerCharacter.UpdateBody(deltaTime);
+        #if UNITY_EDITOR
+        if (Keyboard.current.tKey.wasPressedThisFrame){
+            var ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+            if (Physics.Raycast(ray, out var hit)){
+                Teleport(hit.point);
+            }
         }
+        #endif
     }
 
-    void ApplyFriction()
+    void LateUpdate()
     {
-        Vector3 horizontalVel = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
-        Vector3 frictionForce = -horizontalVel * friction * Time.fixedDeltaTime;
-        rb.linearVelocity += new Vector3(frictionForce.x, 0, frictionForce.z);
+        playerCamera.UpdatePosition(playerCharacter.GetCameraTarget());
     }
 
-    void Jump()
+    public void Teleport(Vector3 position)
     {
-        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
-        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-    }
-
-    void StartSlide()
-    {
-        if (!grounded) return;
-        sliding = true;
-        rb.linearVelocity *= slideBoost;
-    }
-
-    void StopSlide()
-    {
-        sliding = false;
+        playerCharacter.SetPosition(position);
     }
 }
